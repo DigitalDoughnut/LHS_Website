15752d353e468e684f9e63878466d3bf8a709cd5
David Salisbury <davidmsalisbury@hotmail.com>
2017-10-22 12:35:11 +0100

    Initial commit

---
+++ js/myjavascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$("img#Tablet").css({'left': '0%', 'top': '10%', 'width': '24%'});
+//$("img#Tablet").delay(200).animate({'left': ['0%' , 'linear'], 'top': ['10%' , 'linear'], 'width': ['24%' , 'linear']});
+$("img#Mobile").css({'left': '83%', 'top': '23%', 'width': '14.5%'});
+//$("img#Mobile").delay(500).animate({'left': ['83%' , 'linear'], 'top': ['23%' , 'linear'], 'width': ['14.5%' , 'linear']});
+$("img#Tablet2").css({'left': '31%', 'top': '39.5%', 'width': '9.5%'});
+$("img#Mobile2").css({'left': '63%', 'top': '46%', 'width': '4.5%'});
+})
